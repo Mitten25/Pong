cb88b8da7fc6bd9ea1280d53902869c2a4d0196f
Andrew Radford <andrewradford125@gmail.com>
2017-10-27 21:08:59 -0600

    init

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private Vector3 reset;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        reset = new Vector3(0, 6.5f, 0);
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(Random.Range(2f, 3f), 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.transform.name);
+        if(collision.transform.name == "floor" || collision.transform.name == "boundary1" || collision.transform.name == "boundary2")
+        {
+            this.transform.position = reset;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(new Vector3(Random.Range(2f, 3f), 0, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public bool inhit;
+
+    public float speed;
+    public float hit_force;
+    public float drop_speed;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //movement
+        float translation = Input.GetAxis("Horizontal");
+        rb.AddForce(new Vector3(translation, 0, 0) * speed);
+
+        //hit
+        //float hit = Input.GetAxis("Jump");
+        //print(hit);
+        //rb.AddForce(new Vector3(0, hit, 0) * hitforce);
+        if (Input.GetAxis("Jump") > 0 && inhit == false)
+        {
+            inhit = true;
+            rb.AddForce(new Vector3(0, Input.GetAxis("Jump"), 0) * hit_force);
+        }
+        if (inhit == true)
+        {
+            rb.AddForce(new Vector3(0, -1, 0) * drop_speed);
+            if (rb.transform.position.y < 0)
+                inhit = false;
+        }
+        else
+        {
+            rb.transform.position = new Vector3(rb.transform.position.x, 0, rb.transform.position.y);
+        }
+
+
+
+	}
+}

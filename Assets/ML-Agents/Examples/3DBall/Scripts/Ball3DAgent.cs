cb2d2ab7c5f12660fc1d8fe899b243e33ccf81db
LINGXI-XPS\Lingxi <lingxizhong@gmail.com>
2017-10-28 01:20:40 -0600

    Added the weight algorithms

--- Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DAgent.cs
+++ Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DAgent.cs
@@ -4,109 +4,67 @@ using UnityEngine;
 
 public class Ball3DAgent : Agent
 {
-	[Header("Specific to Ball3D")]
+	[Header("Specific to Pong")]
 	public GameObject ball;
-    
+	public float invertMult;
+
 	public override List<float> CollectState()
 	{
 		List<float> state = new List<float>();
-		state.Add(gameObject.transform.rotation.z);
-		state.Add(gameObject.transform.rotation.x);
-		state.Add((ball.transform.position.x - gameObject.transform.position.x) / 5f); // Distance of ball 
-		state.Add((ball.transform.position.y - gameObject.transform.position.y) / 5f);
-		state.Add((ball.transform.position.z - gameObject.transform.position.z) / 5f);
-		state.Add(ball.transform.GetComponent<Rigidbody>().velocity.x / 5f); // The platform getting the ball velocity (x)
-		state.Add(ball.transform.GetComponent<Rigidbody>().velocity.y / 5f);
-		state.Add(ball.transform.GetComponent<Rigidbody>().velocity.z / 5f);
+		state.Add(invertMult * gameObject.transform.position.x / 8f);
+		state.Add(gameObject.transform.position.y / 2f);
+		state.Add(invertMult * gameObject.GetComponent<Rigidbody>().velocity.x / 10f);
+		state.Add(gameObject.GetComponent<Rigidbody>().velocity.y / 10f);
+
+		state.Add(invertMult * ball.transform.position.x / 8f);
+		state.Add(ball.transform.position.y / 8f);
+		state.Add(invertMult * ball.GetComponent<Rigidbody>().velocity.x / 10f);
+		state.Add(ball.GetComponent<Rigidbody>().velocity.y / 10f);
 		return state;
 	}
 
 	// to be implemented by the developer
 	public override void AgentStep(float[] act)
 	{
-		if (brain.brainParameters.actionSpaceType == StateType.continuous)
+		float moveX = 0.0f;
+		if (act[0] == 0f)
 		{
-			float action_z = act[0];
-			if (action_z > 2f)
-			{
-				action_z = 2f;
-			}
-			if (action_z < -2f)
-			{
-				action_z = -2f;
-			}
-			if ((gameObject.transform.rotation.z < 0.25f && action_z > 0f) ||
-				(gameObject.transform.rotation.z > -0.25f && action_z < 0f))
-			{
-				gameObject.transform.Rotate(new Vector3(0, 0, 1), action_z);
-			}
-			float action_x = act[1];
-			if (action_x > 2f)
-			{
-				action_x = 2f;
-			}
-			if (action_x < -2f)
-			{
-				action_x = -2f;
-			}
-			if ((gameObject.transform.rotation.x < 0.25f && action_x > 0f) ||
-				(gameObject.transform.rotation.x > -0.25f && action_x < 0f))
-			{
-				gameObject.transform.Rotate(new Vector3(1, 0, 0), action_x);
-			}
-				
+			moveX = invertMult * -0.25f;
+		}
+		if (act[0] == 1f)
+		{
+			moveX = invertMult * 0.25f;
+		}
+		if (act[0] == 2f)
+		{
+			moveX = 0.0f;
+		}
+		if (act[0] == 3f)
+		{
+			//moveY = 0.5f; // Replace with hit code
+		}
+		gameObject.transform.position = new Vector3(gameObject.transform.position.x + moveX, gameObject.transform.position.y, 5f);
 
-			if (done == false)
-			{
-				reward = 0.1f;
-			}
-		}
-		else
+		if (gameObject.transform.position.x > (invertMult) * 11f)
 		{
-			int action = (int)act[0];
-			if (action == 0 || action == 1)
-			{
-				action = (action * 2) - 1;
-				float changeValue = action * 2f;
-				if ((gameObject.transform.rotation.z < 0.25f && changeValue > 0f) ||
-					(gameObject.transform.rotation.z > -0.25f && changeValue < 0f))
-				{
-					gameObject.transform.Rotate(new Vector3(0, 0, 1), changeValue);
-				}
-			}
-			if (action == 2 || action == 3)
-			{
-				action = ((action - 2) * 2) - 1;
-				float changeValue = action * 2f;
-				if ((gameObject.transform.rotation.x < 0.25f && changeValue > 0f) ||
-					(gameObject.transform.rotation.x > -0.25f && changeValue < 0f))
-				{
-					gameObject.transform.Rotate(new Vector3(1, 0, 0), changeValue);
-				}
-			}
-			if (done == false)
-			{
-				reward = 0.1f;
-			}
+			gameObject.transform.position = new Vector3(-(invertMult) * 11f, gameObject.transform.position.y, 5f);
 		}
-		if ((ball.transform.position.y - gameObject.transform.position.y) < -2f ||
-			Mathf.Abs(ball.transform.position.x - gameObject.transform.position.x) > 3f ||
-			Mathf.Abs(ball.transform.position.z - gameObject.transform.position.z) > 3f)
+		if (gameObject.transform.position.x < (invertMult) * 2f)
 		{
-			done = true;
-			reward = -1f;
+			gameObject.transform.position = new Vector3((invertMult) * 2f, gameObject.transform.position.y, 5f);
 		}
-
+		
+		/*if (gameObject.transform.position.y > -2f)
+		{
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0f, 5f);
+		}*/
 	}
 
 	// to be implemented by the developer
 	public override void AgentReset()
 	{
-        
-		//gameObject.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
-		//gameObject.transform.Rotate(new Vector3(1, 0, 0), Random.Range(-10f, 10f));
-		//gameObject.transform.Rotate(new Vector3(0, 0, 1), Random.Range(-10f, 10f));
-		//ball.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
-		//ball.transform.position = new Vector3(Random.Range(-1.5f, 1.5f), 4f, Random.Range(-1.5f, 1.5f)) + gameObject.transform.position;
+		invertMult = 1f;
+		gameObject.transform.position = new Vector3((invertMult) * 7f, -1.5f, 5f);
+		gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
 	}
 }
